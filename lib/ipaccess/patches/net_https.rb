3062c50a43daa529096a1eb905ed677fd51ae0bc
Paweł Wilk <siefca@gnu.org>
2009-10-03 13:44:58 +0200

    Added files required for easy inclusion of HTTPS-enabled Net::HTTP.

---
+++ lib/ipaccess/patches/net_https.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+#
+# Author::    Paweł Wilk (mailto:pw@gnu.org)
+# Copyright:: Copyright (c) 2009 Paweł Wilk
+# License::   This program is licensed under the terms of {GNU Lesser General Public License}[link:docs/LGPL-LICENSE.html] or {Ruby License}[link:docs/COPYING.html].
+# 
+# Modules contained in this file are meant for
+# patching Ruby's Net::HTTP class in order to add
+# IP access control to it. It is also used
+# to create variant of Net::HTTP class
+# with IP access control.
+# 
+#--
+# 
+# Copyright (C) 2009 by Paweł Wilk. All Rights Reserved.
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either: 1) the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version; or 2) Ruby's License.
+# 
+# See the file COPYING for complete licensing information.
+#
+#++
+# 
+
+require 'net/https'
+require 'ipaccess/patches/net/http'
+
