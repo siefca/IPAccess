8927d0e4474d4faa2538b5c05aab748d2874165e
Paweł Wilk <siefca@gnu.org>
2009-10-06 13:40:56 +0200

    Added example of arming TCPSocket.

--- examples/tcp_server.rb
+++ examples/tcp_server.rb
@@ -5,6 +5,17 @@ require 'ipaccess/socket'
 acl = IPAccess.new
 acl.input.blacklist :strange
 
+# 0
+
+serv = IPAccess::TCPServer.new(31337)     # create listening TCP socket
+serv.acl = :private                       # create and use private access set
+serv.blacklist :local, :private           # block local and private IP addresses
+serv.permit '127.0.0.5'                   # make an exception
+
+puts serv.acl.show                        # show listed IP addresses
+sock = serv.sysaccept                     # accept connection
+
+
 # 1
 
 # Create and use the TCP socket
