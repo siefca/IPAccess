27b0545884c616d141220c5817837fb3359911f5
Paweł Wilk <siefca@gnu.org>
2013-12-26 03:02:20 +0100

    Open-uri example simplified

--- examples/open-uri.rb
+++ examples/open-uri.rb
@@ -1,13 +1,15 @@
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
+require 'net/http'
 require 'ipaccess/net/http'
 require 'open-uri'
 
 # Add host's IP by to black list of global output access set
-IPAccess::Set::Global.output.blacklist 'example.org'
+IPAccess::Set::Global.output.blacklist :unusual
 
 # Arm sockets
 IPAccess.arm Net::HTTP
+puts IPAccess::Set::Global.output.show
 
 # Open URI
-open 'http://example.org/'
+open 'http://localhost/'
