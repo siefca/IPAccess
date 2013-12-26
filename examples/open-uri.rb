f349435979ef92105fd559ca4f40078bb1919c67
Paweł Wilk <siefca@gnu.org>
2013-12-26 03:28:40 +0100

    Open-uri example updated

--- examples/open-uri.rb
+++ examples/open-uri.rb
@@ -9,6 +9,14 @@ IPAccess::Set::Global.output.blacklist :unusual
 
 # Arm sockets
 IPAccess.arm Net::HTTP
+
+# Set some defaults
+Net::HTTP.ipaccess_defaults = {
+  :check_only_real => true,
+  :opened_on_deny  => true
+}
+
+# Show blacklisted IP addresses
 puts IPAccess::Set::Global.output.show
 
 # Open URI
